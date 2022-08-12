e2d48feb1e44d4de2ede1797abf92a919ac82d46
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2022-08-12 11:34:26 +0700

    Test-08

--- js/button.js
+++ js/button.js
@@ -23,3 +23,63 @@ btnPlusElement.onclick = function() {
     showSubMenu.classList.toggle('show-sub-menu');
 }
 
+var itemMenuMobile = document.querySelectorAll('.item-mobile h6');
+var btnMobile = document.querySelectorAll('.item-mobile i');
+
+document.querySelector('.fa-house').onclick = function() {
+    for(var i = 0; i < itemMenuMobile.length; ++i) {
+        if(itemMenuMobile[i].textContent !== 'Trang chủ') {
+            itemMenuMobile[i].classList.remove('title-show');
+            btnMobile[i].classList.remove('line-hight');
+        }
+    }
+    document.querySelector('.home-title').classList.add('title-show');
+    for(var  i = 0; i < btnMobile.length; ++i) {
+        btnMobile[i].classList.add('line-hight');
+    }
+    document.querySelector('.fa-house').classList.remove('line-hight');
+}
+
+document.querySelector('.fa-shop').onclick = function() {
+    for(var i = 0; i < itemMenuMobile.length; ++i) {
+        if(itemMenuMobile[i].textContent !== 'Market') {
+            itemMenuMobile[i].classList.remove('title-show');
+            btnMobile[i].classList.remove('line-hight');
+        }
+    }
+    document.querySelector('.market-title').classList.add('title-show');
+    for(var  i = 0; i < btnMobile.length; ++i) {
+        btnMobile[i].classList.add('line-hight');
+    }
+    document.querySelector('.fa-shop').classList.remove('line-hight');
+}
+
+document.querySelector('.fa-message').onclick = function() {
+    for(var i = 0; i < itemMenuMobile.length; ++i) {
+        if(itemMenuMobile[i].textContent !== 'Chat') {
+            itemMenuMobile[i].classList.remove('title-show');
+            btnMobile[i].classList.remove('line-hight');
+        }
+    }
+    document.querySelector('.chat-title').classList.add('title-show');
+    for(var  i = 0; i < btnMobile.length; ++i) {
+        btnMobile[i].classList.add('line-hight');
+    }
+    document.querySelector('.fa-message').classList.remove('line-hight');
+}
+
+document.querySelector('.fa-th-list').onclick = function() {
+    for(var i = 0; i < itemMenuMobile.length; ++i) {
+        if(itemMenuMobile[i].textContent !== 'Tiện ích') {
+            itemMenuMobile[i].classList.remove('title-show');
+            btnMobile[i].classList.remove('line-hight');
+        }
+    }
+    document.querySelector('.utilities-title').classList.add('title-show');
+    for(var  i = 0; i < btnMobile.length; ++i) {
+        btnMobile[i].classList.add('line-hight');
+    }
+    document.querySelector('.fa-th-list').classList.remove('line-hight');
+}
+
+
